5d1a91591c8ae8e4d68ec144840b376455196e52
CarvingMan <130010242+CarvingMan@users.noreply.github.com>
2024-08-03 03:48:31 +0900

    .

--- Assets/Scripts/Game/BoxObject.cs
+++ Assets/Scripts/Game/BoxObject.cs
@@ -1,6 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
+//using System.Diagnostics;
 using UnityEngine;
 
 public class BoxObject : InteractiveObject
@@ -8,10 +8,10 @@ public class BoxObject : InteractiveObject
     // 상자_0 = 열린거, 상자_1 = 상자 열린거에 책 있는 상태 , 상자_2 = 상자 닫혀 있는거
     protected override void DoInteract()
     {
-        if(isInteractable == false)
+        if (isInteractable == false)
         {
             Debug.Log("상자가 연다");
-            
+
 
         }
         else
@@ -22,10 +22,11 @@ public class BoxObject : InteractiveObject
         //상자가 열린 상태
         if (isInteracted == true)
         {
-            Debug.Log("상자 안에 일기장을 클릭 가능")
+            Debug.Log("상자 안에 일기장을 클릭 가능");
         }
         else
         {
-            Debug.Log("상자 안에 ")
+            Debug.Log("상자 안에 ");
         }
+    }
 }
