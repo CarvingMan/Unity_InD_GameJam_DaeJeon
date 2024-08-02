dd05abdded33fbca6e9b0313564af3abccb92f3c
jooseung <jsedwardyou@gmail.com>
2024-08-03 01:23:09 +0900

    add dialogues and etc.

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -2,5 +2,19 @@ using Eshikivo.DesignPatterns;
 
 public class GameManager: Singleton<GameManager>
 {
-    
+    public void InitializeGame()
+    {
+    }
+
+    public void StartGame()
+    {
+    }
+
+    public void StopGame()
+    {
+    }
+
+    public void StartStage(int stage)
+    {
+    }
 }
