7fbd7e58347d88c33acbf65032928e12ee410fb9
CarvingMan <130010242+CarvingMan@users.noreply.github.com>
2024-08-03 06:27:05 +0900

    .
    ㄴㄹ

---
+++ Assets/Scripts/Objects/DiaryBox.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+namespace Game
+{
+    //책상 밑 일기장 박스 스크립트
+    public class DiaryBox : InteractiveObject
+    {
+
+        [SerializeField]
+        GameObject m_objTextBox = null;
+
+        private void Start()
+        {
+            if(m_objTextBox != null)
+            {
+                m_objTextBox.SetActive(false);
+            }
+            else
+            {
+                Debug.LogError("m_objTextBox가 없습니다.");
+            }
+        }
+
+        protected override void DoInteract()
+        {
+            Debug.Log("INTERACT!!!");
+        }
+
+        public override void OnMouseHoverEnter()
+        {
+            base.OnMouseHoverEnter();
+            m_objTextBox.SetActive(true);
+        }
+
+        public override void OnMouseHoverExit()
+        {
+            base.OnMouseHoverExit();
+            m_objTextBox.SetActive(false);
+        }
+    }
+}
