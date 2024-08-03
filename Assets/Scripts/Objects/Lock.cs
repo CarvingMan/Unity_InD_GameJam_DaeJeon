a491f32d7fddd546eae1cf9a3a85deee4620aaca
seunggeunsavefile <yoonsg0124@gmail.com>
2024-08-03 21:47:04 +0900

    '''
    '''

--- Assets/Scripts/Objects/Lock.cs
+++ Assets/Scripts/Objects/Lock.cs
@@ -8,6 +8,16 @@ namespace Game
         private GameObject m_objLock = null; // 잠금 오브젝트
         private bool isLocked = true; // 잠금 상태 추가
 
+        [SerializeField]
+        private Camera m_camera = null;
+
+        // 플레이어 제어용 스크립트 변수
+        private PlayerControl m_csPlayerControl = null;
+
+        // Closet 참조 추가
+        [SerializeField]
+        private Closet m_closet = null; // Closet 클래스의 인스턴스를 참조
+
         private void Start()
         {
             // m_objLock을 비활성화
@@ -19,6 +29,11 @@ namespace Game
             {
                 Debug.LogError("m_objLock가 없습니다.");
             }
+
+            if (m_csPlayerControl == null)
+            {
+                m_csPlayerControl = FindObjectOfType<PlayerControl>();
+            }
         }
 
         // 잠금을 해제하는 메소드
@@ -28,11 +43,27 @@ namespace Game
             {
                 m_objLock.SetActive(false); // 요청에 따라 비활성화
                 isLocked = false; // 잠금 상태 업데이트
+
+                // Closet의 m_objTextCloset 비활성화
+                if (m_closet != null)
+                {
+                    m_closet.SetTextClosetInactive(); // 메소드 호출
+                }
             }
         }
 
         protected override void DoInteract()
         {
+            if (!m_objLock.activeSelf)
+            {
+                if (m_camera != null)
+                {
+                    m_objLock.transform.position = new Vector2(m_camera.transform.position.x, m_camera.transform.position.y);
+                    m_objLock.SetActive(true);
+                    m_csPlayerControl.SetPlayerStop(true);
+                }
+            }
+
             if (!isLocked) // 잠금 해제된 경우에만 활성화
             {
                 m_objLock.SetActive(false); // 여기서 활성화 요청
@@ -40,22 +71,6 @@ namespace Game
             else
             {
                 m_objLock.SetActive(true); // 여기서 활성화 요청
-            }
-        }
-
-        private void Update()
-        {
-            // 마우스 클릭 감지
-            if (Input.GetMouseButtonDown(0))
-            {
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
-
-                // 클릭한 위치가 Lock 오브젝트가 아닐 경우
-                if (hit.collider == null || hit.collider.gameObject != m_objLock)
-                {
-                    DeactivateLock(); // Lock 오브젝트 비활성화
-                }
             }
         }
 
