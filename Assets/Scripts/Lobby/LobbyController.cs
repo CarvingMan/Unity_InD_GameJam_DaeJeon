f1bbe910ce28af547b0294c0871135ed08fe35a2
jooseung <jsedwardyou@gmail.com>
2024-08-03 15:26:00 +0900

    work on lobby

--- Assets/Scripts/Lobby/LobbyController.cs
+++ Assets/Scripts/Lobby/LobbyController.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using Febucci.UI;
 using UnityEngine;
 using DG.Tweening;
+using UnityEditor;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.SceneManagement;
 
 public class LobbyController : MonoBehaviour
 {
@@ -13,18 +16,63 @@ public class LobbyController : MonoBehaviour
     [SerializeField] private string[] introPlayer;
     [SerializeField] private string[] introButterfly;
 
+    [SerializeField] private SpriteRenderer startIcon;
     [SerializeField] private SpriteRenderer butterfly;
+
+    [SerializeField] private GameObject light;
+    [SerializeField] private Light2D global;
+    [SerializeField] private Camera cam;
+
+    private bool _introStarted = false;
     
     private void Start()
     {
-        StartCoroutine(StartIntro());
-    }
-
-    private IEnumerator StartIntro()
-    {
+        global.gameObject.SetActive(false);
         var color = butterfly.color;
         color.a = 0;
         butterfly.color = color;
+    }
+
+    private void Update()
+    {
+        if (_introStarted) return;
+
+        var mousePosition = Input.mousePosition;
+        var worldPosition = cam.ScreenToWorldPoint(mousePosition);
+        worldPosition.z = 0;
+        light.transform.position = worldPosition;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            var collider = startIcon.GetComponent<Collider2D>();
+            if (collider.OverlapPoint(worldPosition))
+            {
+                StartIntro();
+            }
+        }
+    }
+
+    private void StartIntro()
+    {
+        StartCoroutine(StartIntroCo());
+        _introStarted = true;
+    }
+
+    private IEnumerator StartIntroCo()
+    {
+        var spotLight = light.GetComponent<Light2D>();
+
+        while (spotLight.intensity > 0.01f)
+        {
+            spotLight.intensity -= Time.deltaTime * 0.25f;
+            yield return null;
+        }
+
+        spotLight.intensity = 0;
+        
+        light.gameObject.SetActive(false);
+        global.gameObject.SetActive(true);
+        startIcon.gameObject.SetActive(false);
         
         foreach (var intro in introPlayer)
         {
@@ -44,7 +92,7 @@ public class LobbyController : MonoBehaviour
         yield return new WaitWhile(tween.IsPlaying);
         yield return new WaitForSeconds(2);
 
-        butterfly.DOFade(0.75f, 1.5f);
+        // butterfly.DOFade(0.75f, 1.5f);
 
         foreach (var intro in introButterfly)
         {
@@ -63,8 +111,17 @@ public class LobbyController : MonoBehaviour
         tween = butterfly.DOFade(0, 2.5f);
         yield return new WaitWhile(tween.IsPlaying);
 
-        Debug.Log("Done");
+        if (SceneManager.sceneCount < 2)
+        {
+            Debug.LogError("Not enough scenes");
+            yield break;
+        }
 
-        yield return null;
+        LoadMain();
+    }
+
+    private void LoadMain()
+    {
+        SceneManager.LoadScene(1);
     }
 }
